aec4713648a6041394d7df1fe8f958480b2aa471
ThalitaFernandes <dc03934@gmail.com>
2023-03-18 12:41:39 -0300

    Created project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eletronic.prescription',
+  appName: 'eletronic-prescription',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
